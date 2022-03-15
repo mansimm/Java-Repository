28d2cc655fd82e4605a031d5cc05aa76ed5352cc
Mansi <43803851+mansimm@users.noreply.github.com>
2022-03-15 19:59:49 +0530

    Update JdbcDemo.java

--- JDBC/Simple Connection/JdbcDemo.java
+++ JDBC/Simple Connection/JdbcDemo.java
@@ -20,10 +20,15 @@ public class JdbcDemo {
 			
 			ResultSet rs = statement.executeQuery("Select * from Users");
 			
+			System.out.println("Ninjas present in sqad 7 :");
 			while(rs.next())
 			{
 				System.out.println("Id: "+rs.getInt(1)+" Name: "+rs.getString(2)+" Age: "+rs.getInt(3));
 			}
+			
+			int x = statement.executeUpdate("delete from Users where user_id=2");
+			System.out.println("Number of eliminated Ninjas: "+x);
+
 			conn.close();
 			statement.close();
 			rs.close();
